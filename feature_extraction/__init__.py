bb96f65e3cfa5a70ee7c2cf1fe5ffc1ca856345f
Andrew Sonin <sonin.cel@gmail.com>
2021-07-25 18:17:47 +0300

    Add named entites stats calculation

--- feature_extraction/__init__.py
+++ feature_extraction/__init__.py
@@ -56,12 +56,8 @@ _tagset = (
 )
 
 
-def count_part_of_speech(text: str) -> Dict[str, Union[int, float]]:
+def generate_text_features(text: str) -> Dict[str, Union[int, float]]:
     total_count = dict.fromkeys(_tagset, 0)
-    max_in_sent = dict.fromkeys(_tagset, 0)
-    min_in_sent = dict.fromkeys(_tagset, 0)
-    mean_in_sent = dict.fromkeys(_tagset, 0)
-
     tokenized_text = nltk.word_tokenize(text)
     inv_text_len = 1 / len(tokenized_text)
     for word, pos in nltk.pos_tag(tokenized_text):
@@ -70,13 +66,17 @@ def count_part_of_speech(text: str) -> Dict[str, Union[int, float]]:
         if general_tag in _general_tags:
             total_count[general_tag] += inv_text_len
 
+    max_in_sent = dict.fromkeys(_tagset, 0)
+    min_in_sent = dict.fromkeys(_tagset, 0)
+    mean_in_sent = dict.fromkeys(_tagset, 0)
+    general_tags = set()
+    tags = set()
+
     sentences = nltk.sent_tokenize(text)
     num_sentences = len(sentences)
     num_words = []
     words_len = []
 
-    ps = set()
-    general_ps = set()
     for sentence in map(nltk.word_tokenize, sentences):
         cur_sentence_stat = dict.fromkeys(_tagset, 0)
         num_words.append(len(sentence))
@@ -84,10 +84,10 @@ def count_part_of_speech(text: str) -> Dict[str, Union[int, float]]:
         for word, pos in nltk.pos_tag(sentence):
             words_len.append(len(word))
             cur_sentence_stat[pos] += inv_sent_len
-            ps.add(pos)
+            tags.add(pos)
             general_tag = f'g{pos[:2]}'
             if general_tag in _general_tags:
-                general_ps.add(general_tag)
+                general_tags.add(general_tag)
                 cur_sentence_stat[general_tag] += inv_sent_len
         for tag in _tagset:
             max_in_sent[tag] = max(max_in_sent[tag], cur_sentence_stat[tag])
@@ -106,12 +106,16 @@ def count_part_of_speech(text: str) -> Dict[str, Union[int, float]]:
 
     num_words = np.array(num_words)
     words_len = np.array(words_len)
-    res['NUM_SENTENCES'] = num_sentences
+    res['NUM_SENTENCES'] = len(num_words)
     res['MEAN_NUM_WORDS'] = num_words.mean()
     res['STD_NUM_WORDS'] = num_words.std()
     res['NUM_WORDS'] = len(words_len)
     res['MEAN_WORD_LEN'] = words_len.mean()
     res['STD_WORD_LEN'] = words_len.std()
-    res['PS_UNIQUE'] = len(ps)
-    res['gPS_UNIQUE'] = len(general_ps)
+    res['TAGS_UNIQUE'] = len(tags)
+    res['GENERAL_TAGS_UNIQUE'] = len(general_tags)
+
+    named_entities = get_named_entities(text)
+    res['NAMED_ENTITIES_PER_SENTENCE'] = len(named_entities) / num_sentences
+    res['UNIQUE_NAMED_ENTITIES_PER_SENTENCE'] = len(set(named_entities)) / num_sentences
     return res
